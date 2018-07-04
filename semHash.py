a5c61bc0a929e552c37671b69ab12542026fda53
Rive Sunder <rivesunder@protonmail.com>
2018-07-04 09:18:52 +0100

    update openai LSTM wrapper for saving full vector of representations

--- semHash.py
+++ semHash.py
@@ -15,7 +15,7 @@ FLAGS = tf.app.flags.FLAGS
 #tf.app.flags.DEFINE_boolean('restore', False,"""Restore previously trained model""")
 tf.app.flags.DEFINE_integer('maxSteps', 100,"""number of epochs""")
 tf.app.flags.DEFINE_integer('dispIt', 20,"""display every nth iteration""")
-tf.app.flags.DEFINE_integer('batchSize', 8,"""Number of entries per minibatch""")
+tf.app.flags.DEFINE_integer('batchSize', 45,"""Number of entries per minibatch""")
 tf.app.flags.DEFINE_float('lR', 3e-7,"""learning rate""")
 tf.app.flags.DEFINE_boolean('dispFigs', False,"""Whether to save figures""")
 tf.app.flags.DEFINE_float('dORate', 0.125,"""rate for applying dropout""")
@@ -29,12 +29,23 @@ lR = FLAGS.lR
 dispFigs = FLAGS.dispFigs
 dORate = FLAGS.dORate
 
-myData = np.load('./data/XBio.npy')
-# Each word frequency entry is a feature, so we should probably normalize them all to be similar
+if(0):
+    myData = np.load('./data/XBio.npy')
+    
+    #myData = myData.T
+elif(0):
+    myData = np.load('./data/myRaman.npy')
+elif(1):
+    myData = np.load('./data/openaiVecJobDescriptions.npy')
+
+print(myData.shape,np.mean(myData),np.min(myData),np.max(myData))
+
+# Each word frequency entry is a feature, so we should probably normalize them all to be similar?
 #for cj in range(len(myData)):
 #    myData[cj,:] = myData[cj,:] / (np.max(myData[cj,:])+1e-2)
-print(np.mean(myData),np.min(myData),np.max(myData))
-myData = myData.T # examples in first dimension
+
+#myData = myData.T # examples in first dimension
+
 np.random.seed(42)
 myVal = myData[0:5,:]
 myTrain = myData[5:len(myData)-1,:]
@@ -46,34 +57,34 @@ data = tf.placeholder("float",[None,lenData], name='data')
 
 h1Size = 1024
 h2Size = 512
-codeSize = 64
+codeSize = 128
 #display dimensions for encodings
-dimX= 8   
+dimX= 16  
 dimY = 8
 
 def semHash(data):
     """# A fully-connected autoencoder for semantic hashing vector representations of documents"""
 
-    fc1 = tf.layers.dense(data, h1Size, activation=tf.nn.leaky_relu)
+    fc1 = tf.layers.dense(data, h1Size, activation=tf.nn.sigmoid)
     do1 = tf.nn.dropout(fc1,(1-dORate))
 
-    fc2 = tf.layers.dense(do1, h2Size, activation=tf.nn.leaky_relu)
+    fc2 = tf.layers.dense(do1, h2Size, activation=tf.nn.sigmoid)
     do2 = tf.nn.dropout(fc2,(1-dORate))
 
-    fc3 = tf.layers.dense(do2, h2Size, activation=tf.nn.leaky_relu)
+    fc3 = tf.layers.dense(do2, h2Size, activation=tf.nn.sigmoid)
     do3 = tf.nn.dropout(fc3,(1-dORate))
 
-    codes = tf.layers.dense(do3, codeSize, activation=tf.nn.leaky_relu)
+    codes = tf.layers.dense(do3, codeSize, activation=tf.nn.sigmoid)
     do4 = tf.nn.dropout(codes,(1-dORate))
 
-    fc5 = tf.layers.dense(do4, h2Size, activation=tf.nn.leaky_relu)
+    fc5 = tf.layers.dense(do4, h2Size, activation=tf.nn.sigmoid)
     do5 = tf.nn.dropout(fc5,(1-dORate))
 
-    fc6 = tf.layers.dense(do5, h2Size, activation=tf.nn.leaky_relu)
+    fc6 = tf.layers.dense(do5, h2Size, activation=tf.nn.sigmoid)
     do6 = tf.nn.dropout(fc6,(1-dORate))
 
    
-    fc7 = tf.layers.dense(do6, h1Size, activation=tf.nn.leaky_relu)
+    fc7 = tf.layers.dense(do6, h1Size, activation=tf.nn.sigmoid)
     do7 = tf.nn.dropout(fc7,(1-dORate))
     
     rebow = tf.layers.dense(do7,lenData)
@@ -97,7 +108,9 @@ print("output shape rebow and codes: ", rebow.shape,codes.shape)
 # objective function and training
 
 # MSE
-loss = ((tf.pow(rebow-data, 2)))
+loss = tf.losses.mean_squared_error(data,rebow)
+
+#loss = ((tf.pow(rebow-data, 2)))
 
 trainOp = tf.train.AdamOptimizer(
 	learning_rate=lR,beta1=0.9,
@@ -121,7 +134,7 @@ def main(unused_argv):
 
             inp = tf.placeholder(tf.float32)
             myMean = tf.reduce_mean(inp)
-
+            np.random.shuffle(myTrain)
             for cm in range(0,batchSize,len(myTrain)):
                 input_ = myTrain[ck:ck+batchSize,:]
                 #print(input_.shape)
@@ -129,65 +142,69 @@ def main(unused_argv):
                 #print("batch training loss: %.3e"%myMean.eval(feed_dict={inp:batchLoss}))
 
 
-                if (ck%dispIt == 0):
-                    #report training and validation losses
-                    #mySaver.save(sess,'./models/'+myModel,global_step=i)\
+            if (ck%dispIt == 0):
+                #report training and validation losses
+                #mySaver.save(sess,'./models/'+myModel,global_step=i)\
 
-                    # Get training and validation loss scores
-                    input_ = myTrain
-                    inVal_ = myVal
+                # Get training and validation loss scores
+                input_ = myTrain
+                inVal_ = myVal
 
-                    myTemp = (sess.run(loss, feed_dict={data: input_}))
-                    myLossTrain = myMean.eval(feed_dict={inp: myTemp})
+                myTemp = (sess.run(loss, feed_dict={data: input_}))
+                myLossTrain = myMean.eval(feed_dict={inp: myTemp})
 
-                    myTemp = (sess.run(loss, feed_dict={data: inVal_}))
-                    myLossVal = myMean.eval(feed_dict={inp: myTemp})
-                    elapsed = time.time()-t0
-                    print("Epoch %i %.2f s elapsed, training loss, validation loss: %.4e, %.4e"%(ck,elapsed,myLossTrain,myLossVal))
+                myTemp = (sess.run(loss, feed_dict={data: inVal_}))
+                myLossVal = myMean.eval(feed_dict={inp: myTemp})
+                elapsed = time.time()-t0
+                print("Epoch %i %.2f s elapsed, training loss, validation loss: %.4e, %.4e"%(ck,elapsed,myLossTrain,myLossVal))
 
-                    [rebowT, codesT] = sess.run([rebow,codes],feed_dict = {data: input_})
-                    [rebowV, codesV] = sess.run([rebow,codes],feed_dict = {data: inVal_})
+                [rebowT, codesT] = sess.run([rebow,codes],feed_dict = {data: input_})
+                [rebowV, codesV] = sess.run([rebow,codes],feed_dict = {data: inVal_})
 
-                    meanCodes = tf.reduce_mean(codesT,axis=0)
-                    if(1):#dispFigs):
-                        # Display figures of progress
-                        plt.figure(figsize=(20,20))
-                        cl = 10
-                        plt.subplot(3,3,1)
-                        plt.plot(input_[cl,:])
-                        plt.title("input Bag o' Words",fontsize=18)
-                        plt.ylabel("training",fontsize=32)
-                        plt.subplot(3,3,2)
-                        plt.imshow(np.reshape(codesT[cl,:],(dimX,dimY)))
-                        plt.title("encoding",fontsize=18)
-                        plt.subplot(3,3,3)
-                        plt.plot(rebowT[cl,:])
-                        plt.title("decoded Bag o' Words",fontsize=18)
-                        
-                        cl = 20
-                        plt.subplot(3,3,4)
-                        plt.plot(input_[cl,:])
-                        plt.title("input Bag o' Words",fontsize=18)
-                        plt.ylabel("training",fontsize=32)
-                        plt.subplot(3,3,5)
-                        plt.imshow(np.reshape(codesT[cl,:],(dimX,dimY)))
-                        plt.title("encoding",fontsize=18)
-                        plt.subplot(3,3,6)
-                        plt.plot(rebowT[cl,:])
-                        plt.title("decoded Bag o' Words",fontsize=18)
-                        
-                        cl = 3
-                        plt.subplot(3,3,7)
-                        plt.plot(inVal_[cl,:])
-                        plt.title("input Bag o' Words",fontsize=18)
-                        plt.ylabel("validation",fontsize=32)
-                        plt.subplot(3,3,8)
-                        plt.imshow(np.reshape(codesV[cl,:],(dimX,dimY)))
-                        plt.title("encoding",fontsize=18)
-                        plt.subplot(3,3,9)
-                        plt.plot(rebowV[cl,:])
-                        plt.title("decoded Bag o' Words",fontsize=18)
-                        plt.savefig('./figs/trainingSemHash%i.png'%ck)
+                meanCodes = tf.reduce_mean(codesT,axis=0)
+                if(1):#dispFigs):
+                    # Display figures of progress
+                    plt.figure(figsize=(20,20))
+                    cl = 10
+                    plt.subplot(3,3,1)
+                    plt.plot(input_[cl,:])
+                    plt.title("input Bag o' Words",fontsize=18)
+                    plt.ylabel("training",fontsize=32)
+                    plt.subplot(3,3,2)
+                    plt.imshow(np.reshape(codesT[cl,:],(dimX,dimY)))
+                    plt.title("encoding",fontsize=18)
+                    plt.subplot(3,3,3)
+                    plt.plot(rebowT[cl,:])
+                    plt.title("decoded Bag o' Words",fontsize=18)
+                    
+                    cl = 20
+                    plt.subplot(3,3,4)
+                    plt.plot(input_[cl,:])
+                    plt.title("input Bag o' Words",fontsize=18)
+                    plt.ylabel("training",fontsize=32)
+                    plt.subplot(3,3,5)
+                    plt.imshow(np.reshape(codesT[cl,:],(dimX,dimY)))
+                    plt.title("encoding",fontsize=18)
+                    plt.subplot(3,3,6)
+                    plt.plot(rebowT[cl,:])
+                    plt.title("decoded Bag o' Words",fontsize=18)
+                    
+                    cl = 3
+                    plt.subplot(3,3,7)
+                    plt.plot(inVal_[cl,:])
+                    plt.title("input Bag o' Words",fontsize=18)
+                    plt.ylabel("validation",fontsize=32)
+                    plt.subplot(3,3,8)
+                    plt.imshow(np.reshape(codesV[cl,:],(dimX,dimY)))
+                    plt.title("encoding",fontsize=18)
+                    plt.subplot(3,3,9)
+                    plt.plot(rebowV[cl,:])
+                    plt.title("decoded Bag o' Words",fontsize=18)
+                    plt.savefig('./figs/trainingSemHash%i.png'%ck)
         # save the resulting codes
+
+        np.save('./output/valCodes.npy',codesV)
+        np.save('./output/trainCodes.npy',codesT)
+
 if __name__ == "__main__":
     tf.app.run()
--- text2MeanVector.py
+++ text2MeanVector.py
@@ -15,21 +15,30 @@ myJDs = np.load('../data/myJDs.npy')
 
 print(myJDs.shape)
 myDim = 4096
-
+average_vector = 1
 t0 = time.time()
+X = []
 for ck in range(len(myJDs)):
     t1 = time.time()
     tempX = model.transform(myJDs[ck])
     print(tempX.shape)
     tempX = np.mean(tempX,axis=0)
-    if(ck == 0):
-        X = np.reshape(tempX,(1,myDim))
-    else:
-        X = np.append(X,np.reshape(tempX,(1,myDim)),axis=0)
+    if(average_vector): #switch this conditional to save full vector    
+        if(ck == 0):
+            X = np.reshape(tempX,(1,myDim))
+        else:
+            X = np.append(X,np.reshape(tempX,(1,myDim)),axis=0)
+    else:        
+        X.append(np.array(tempX))
+
     elapsed = time.time()-t1
 
     print('job description number %i parsed in %.3f s'%(ck,elapsed))
 
-np.save('../data/openaiVecJobDescriptions.npy',X)
+if(average_vector):
+    np.save('../data/openaiVecJobDescriptions.npy',X)
+else:
+    np.save('../data/openaiFullVecJobDescriptions.npy',X)
+
 elapsed = time.time()-t0
 print('finished after %.3f s'%elapsed)
